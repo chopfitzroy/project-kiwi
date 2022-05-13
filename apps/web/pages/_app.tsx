fc5702c1d0fc9db805460cf1f1680e3e2510605f
Otis Sutton <wright.a.otis@gmail.com>
2022-05-13 17:28:38 +1200

    feat(init): proof of concept

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "@discretize/gw2-ui-new/dist/default_style.css";
+import "@discretize/gw2-ui-new/dist/index.css";
+import "@discretize/typeface-menomonia";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,37 @@
-import { Button } from "ui";
+import { usePreview } from "./usePreview";
+import { Skill } from "@discretize/gw2-ui-new";
+import { ErrorBoundary } from "react-error-boundary";
+
+const components = { Skill };
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
 
 export default function Web() {
+  const { content, Preview, setContent } = usePreview();
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        // reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <div>
+        <h1>Web</h1>
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+        />
+
+        <Preview components={components} />
+      </div>
+    </ErrorBoundary>
   );
 }
