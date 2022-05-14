ebb43371346f79737e9e0c9e6da61ddfd219358c
Otis Sutton <wright.a.otis@gmail.com>
2022-05-15 10:52:54 +1200

    feat(editor): flesh out tutorial

--- apps/web/pages/editor-tutorial/index.tsx
+++ apps/web/pages/editor-tutorial/index.tsx
@@ -1,9 +1,32 @@
-const EditorTutorial = () => {
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import { readFileSync } from "fs";
+import path from "path";
+import { InferGetStaticPropsType } from "next";
+
+export type EditorTutorialProps = InferGetStaticPropsType<
+  typeof getStaticProps
+>;
+
+type EditorTutorialSignature = (props: EditorTutorialProps) => JSX.Element;
+const EditorTutorial: EditorTutorialSignature = ({ content }) => {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="prose max-w-xl p-4 m-auto">
+      <MDXRemote {...content} />
     </div>
   );
 };
 
+const getStaticProps = async () => {
+  const editorTutorialPath = path.resolve(
+    process.cwd(),
+    "data/editor-tutorial.mdx"
+  );
+  const editorTutorial = readFileSync(editorTutorialPath, "utf8");
+
+  const content = await serialize(editorTutorial);
+  return { props: { content } };
+};
+
+export { getStaticProps };
 export default EditorTutorial;
